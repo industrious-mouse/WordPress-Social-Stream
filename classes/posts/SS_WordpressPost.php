fa1b7db0cab647040af2b98ee34776fa1f0fcb79
Christian Thomas <me@christianthomas.co.uk>
2016-03-16 14:30:35 +0000

    Update SS_WordpressPost.php
    Add Excerpt to WP Post

--- classes/posts/SS_WordpressPost.php
+++ classes/posts/SS_WordpressPost.php
@@ -13,6 +13,7 @@ class SS_WordpressPost extends SS_Post
         return [
             'id'        => $post->ID,
             'content'   => apply_filters('the_content', $post->post_content),
+            'excerpt'   => apply_filters('the_excerpt', $post->post_excerpt),
             'timestamp' => get_the_time('U', $post->ID),
             'permalink' => get_permalink($post->ID)
         ];
