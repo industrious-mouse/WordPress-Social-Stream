83b21e7dbf6b57d871fc21d24aef906861764001
Christian Thomas <me@christianthomas.co.uk>
2016-04-20 14:39:12 +0100

    Update SS_WordpressPost.php

--- classes/posts/SS_WordpressPost.php
+++ classes/posts/SS_WordpressPost.php
@@ -13,11 +13,12 @@ class SS_WordpressPost extends SS_Post
         setup_postdata($post);
 
         $return = [
-            'id'        => get_the_ID(),
+            'id'        => $post->ID,
+            'title'     => get_the_title($post),
             'content'   => get_the_content(),
             'excerpt'   => get_the_excerpt(),
-            'timestamp' => get_the_time('U'),
-            'permalink' => get_permalink()
+            'timestamp' => get_the_time('U', $post),
+            'permalink' => get_permalink($post)
         ];
 
         wp_reset_postdata();
